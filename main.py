6f4d244b24615e6eb5a7b85459cd1ceff596402d
cristinawithout <clhanes@hotmail.com>
2011-06-18 19:14:47 -0700

    Filter out non letters. Choose the number of words to output.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/bin/python
 import sets,random
+import re
 
 myfile = open("bigfile.txt", "r")
 myset = set()
@@ -10,5 +11,14 @@ for line in myfile:
 
 mylist = list(myset)
 
-for x in xrange(0,len(mylist)-1):
- print( mylist[random.randint(0,len(mylist)-1)]),
+newlist = [];
+for x in mylist:
+  myword = re.sub("[^a-zA-Z']","",x)
+  if len(myword) > 2 and len(myword) < 10:
+    newlist.append(myword);
+
+#print newlist;
+print "How many words? ",
+num = int(raw_input())
+for x in range(0,num):
+ print( newlist[random.randint(0,len(newlist)-1)]),
